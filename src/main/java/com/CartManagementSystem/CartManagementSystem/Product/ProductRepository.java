ff9e2db21a337dc76670a07fd19d897f273eb021
ChamikaRohan <chamika707@gmail.com>
2024-05-07 20:17:51 +0530

    Improved env varibles security

--- src/main/java/com/CartManagementSystem/CartManagementSystem/Product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.CartManagementSystem.CartManagementSystem.Product;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    // Define additional methods if needed
-}
